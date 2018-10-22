88444e2ba3b8c36d58b8f0a89dbad330cca66f8b
rhys909 <connorR@cardiff.ac.uk>
2018-10-22 11:39:16 +0100

    Group project final room

---
+++ Final_room.py
@@ -0,0 +1,56 @@
+Main_Title = """
+
+ __  ___  __  .______       __   __       __          _______.    __  ___      ___           _______.___________. __       _______
+|  |/  / |  | |   _  \     |  | |  |     |  |        /       |   |  |/  /     /   \         /       |           ||  |     |   ____|
+|  '  /  |  | |  |_)  |    |  | |  |     |  |       |   (----`   |  '  /     /  ^  \       |   (----`---|  |----`|  |     |  |__
+|    <   |  | |      /     |  | |  |     |  |        \   \       |    <     /  /_\  \       \   \       |  |     |  |     |   __|
+|  .  \  |  | |  |\  \----.|  | |  `----.|  `----.----)   |      |  .  \   /  _____  \  .----)   |      |  |     |  `----.|  |____
+|__|\__\ |__| | _| `._____||__| |_______||_______|_______/       |__|\__\ /__/     \__\ |_______/       |__|     |_______||_______|
+
+"""
+
+end_title = """
+
+.___________. __    __   _______     _______ .__   __.  _______
+|           ||  |  |  | |   ____|   |   ____||  \ |  | |       \
+`---|  |----`|  |__|  | |  |__      |  |__   |   \|  | |  .--.  |
+    |  |     |   __   | |   __|     |   __|  |  . `  | |  |  |  |
+    |  |     |  |  |  | |  |____    |  |____ |  |\   | |  '--'  |
+    |__|     |__|  |__| |_______|   |_______||__| \__| |_______/
+
+"""
+
+death_title = """
+
+____    ____  ______    __    __      _______   __   _______  _______   __   __
+\   \  /   / /  __  \  |  |  |  |    |       \ |  | |   ____||       \ |  | |  |
+ \   \/   / |  |  |  | |  |  |  |    |  .--.  ||  | |  |__   |  .--.  ||  | |  |
+  \_    _/  |  |  |  | |  |  |  |    |  |  |  ||  | |   __|  |  |  |  ||  | |  |
+    |  |    |  `--'  | |  `--'  |    |  '--'  ||  | |  |____ |  '--'  ||__| |__|
+    |__|     \______/   \______/     |_______/ |__| |_______||_______/ (__) (__)
+
+"""
+
+final_room = {
+    "name": "Final Room",
+
+    "description":
+    """
+     As you enter the final room the hot smokey air fills your lungs.
+    The heat dries the moisture in your eyes, blurring your vision.
+    As you look up from the burnt stone floor you see a plethora of
+    skeletons; the remains of the people that have tried before.
+
+     The ground shakes and a hot gust of air blows you back. Towering
+    over you is the grotesque scaley beast with dagger-like fangs and
+    wings too small for its humongous body. Wrapped in its tail is an
+    annoying donkey.
+
+    """,
+
+    "exits": {"north":"Kirill"},
+
+    "items": []
+}
+
+dronkey = 
